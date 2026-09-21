20f6176c776ff263d73abb7e52dac0ddd380a987
rodrigolarae <rodrigo.larae@gmail.com>
2026-09-21 15:45:17 -0300

    Update supabase.js

--- api/_lib/supabase.js
+++ api/_lib/supabase.js
@@ -2,10 +2,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY
 
 if (!supabaseUrl || !supabaseServiceKey) {
-  throw new Error('Faltan variables de entorno SUPABASE_URL o SUPABASE_SERVICE_ROLE_KEY')
+  throw new Error('Faltan variables de entorno SUPABASE_URL o SUPABASE_SERVICE_KEY')
 }
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
